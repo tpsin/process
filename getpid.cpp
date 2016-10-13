169d16b0cf5c9c25874d26fa1ad5f29ce0c2752c
Antonio Pierro <antonio.pierro@gmail.com>
2016-10-13 22:00:17 +0200

    Update getpid.cpp

--- getpid.cpp
+++ getpid.cpp
@@ -6,9 +6,8 @@ int main(void)
    int number;
 
    std::cout << "The PID of this process is :" << getpid() << std::endl;
-
-   // These instructions will block the process waiting for input.
    std::cout << "\nInsert a number: " << std::endl;
+   // The folloeing instruction will block the process waiting for input.
    std::cin >> number;
 
    return 0;
