74a6cda3017971689f42a6bcee2a0bba8edc59d2
Antonio Pierro <antonio.pierro@gmail.com>
2016-10-28 23:08:02 +0200

    Create fork.cpp

---
+++ fork.cpp
@@ -0,0 +1,46 @@
+/**
+
+Fork in C/C++ – a minimum working example.
+A fork create a brand new process. The processes do not share memory.
+
+Author: Antonio Pierro
+
+*/
+
+#include <unistd.h>
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+    int counter = 0;
+    pid_t pid = fork();
+    std::string processName;
+
+    if (pid == 0)
+    {
+        // child process
+        processName = "\033[1;31mChild process: \033[0m ";
+        for (int i = 0; i < 50; ++i)
+        {
+            std::cout << processName << "counter " << ++counter << std::endl;
+        }
+    }
+    else if (pid > 0)
+    {
+        // parent process
+        processName = "\033[1;34mParent process: \033[0m "; //
+        for (int i = 0; i < 50; ++i)
+        {
+            std::cout << processName << "counter " << ++counter << std::endl;
+        }
+    }
+    else
+    {
+        // fork failed
+        std::cout << "fork() failed!" << std::endl;
+        return 1;
+    }
+    std::cout << processName << "TERMINATED" << std::endl;
+
+    return 0;
+}
