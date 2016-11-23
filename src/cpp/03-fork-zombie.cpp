a8d6056db8eaf1e433ce6a5c8a4756ce3e4fa5a0
Antonio Pierro <antonio.pierro@gmail.com>
2016-11-23 18:18:35 +0100

    Update 03-fork-zombie.cpp

--- src/cpp/03-fork-zombie.cpp
+++ src/cpp/03-fork-zombie.cpp
@@ -1,23 +1,25 @@
 /* 
  * Demonstration of zombies.
- * Run in background "g++ 03-zombie.cpp; ./a.out &" and then perform "ps -l"
+ * Run in background "g++ 03-zombie.cpp; ./a.out &" 
+ * and then perform "ps -l"
  */
 
 #include <stdlib.h>
 #include <iostream>
 #include <unistd.h>
+#include <sys/wait.h>
 
 int main ()
 {
     if (fork() == 0) {
-
         /* Child */
         std::cout << "Terminating Child, PID = " << getpid() << std::endl;
         exit(0);
     } else {
+        /* Parent */
         std::cout << "Running Parent, PID = " << getpid() << std::endl;
-        while (1)
-            ; /* Infinite loop */
+        sleep(60);
+        wait(NULL);
     }
 
     return 0;
