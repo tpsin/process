eb7a504341b1dbb2f298fd172692204fa2a18b65
Antonio Pierro <antonio.pierro@gmail.com>
2016-11-23 17:13:28 +0100

    Added multi fork exercise

---
+++ src/cpp/09-multi-fork.cpp
@@ -0,0 +1,35 @@
+/**
+Try to find out what are the possible outputs of the following program.
+____*(18)_____*(19)_________WAIT______A______EXIT
+    |         |
+    |         |_____B____EXIT
+    |
+    |
+    |__0____1____A____EXIT
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main () {
+
+    if (fork() > 0) {
+        if(fork() == 0) {
+            printf("B");
+            exit(0);
+        } else {
+            printf("C");
+			wait(NULL);
+        }
+    } else {
+		for(int c = 0; c < 2; c++) {
+			printf("%d", c);
+		}
+    }
+
+    printf("A");
+
+    return 0;
+}
