67bfee9e6bf09c7c2873ed0632c234a38cf9f939
Antonio Pierro <antonio.pierro@gmail.com>
2016-12-09 22:03:54 +0100

    Added MyThread class which extends Thread class and overridden the run method

--- src/java/threadPackage/ThreadClass.java
+++ src/java/threadPackage/ThreadClass.java
@@ -1,9 +1,21 @@
 package threadPackage;
 
+class MyThread extends Thread {
+
+	public void run() {
+		for(int c = 0; c < 10; c++) {
+			System.out.println("Value " + c);
+		}
+	}
+
+}
+
 public class ThreadClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		MyThread myThread = new MyThread();
+		myThread.start();
 
 	}
 
